3540d43dc0ed93c90aa83e730af3953629479319
ATHEXgod2 <notrelatable.npc@gmail.com>
2024-08-11 01:45:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
+# config.py
 
+API_ID = "YOUR_API_ID"
+API_HASH = "YOUR_API_HASH"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+PRIVATE_CHANNEL_ID = -100xxxxxxxxx
+ADMIN_USER_ID = 123456789
+
+BITLY_ACCESS_TOKEN = "YOUR_BITLY_ACCESS_TOKEN"
+
+UPI_ID = "your_upi_id@bank"
+PREMIUM_AMOUNT = 100
+
+ENABLE_PREMIUM = True
+
+# MongoDB Connection String
+MONGO_URI = "your_mongo_connection_string"
